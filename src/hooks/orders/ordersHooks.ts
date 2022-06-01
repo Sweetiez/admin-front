3df488b1209a349eb37c61e1e1d67214b70e9629
Gwendal <gwendou95@hotmail.fr>
2022-06-01 19:42:19 +0200

    feat(orders): change order status

--- src/hooks/orders/ordersHooks.ts
+++ src/hooks/orders/ordersHooks.ts
@@ -1,6 +1,7 @@
 import { useQuery } from 'react-query';
 import { authenticatedRequest } from '../common/request';
 import OrderModel from '../../components/Orders/models/OrderModel';
+import UpdateOrderStatusRequest from './requests/UpdateOrderStatusRequest';
 
 export function useOrders() {
   return useQuery<OrderModel[], Error>(`all-orders`, async () => {
@@ -9,4 +10,16 @@ export function useOrders() {
     });
     return data;
   });
+}
+
+export async function updateOrderStatus(
+  id: string,
+  request: UpdateOrderStatusRequest,
+) {
+  const { data } = await authenticatedRequest({
+    url: `admin/order/${id}`,
+    method: 'PUT',
+    data: request,
+  });
+  return data;
 }
