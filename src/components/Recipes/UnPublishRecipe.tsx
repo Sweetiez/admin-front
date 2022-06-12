2bc1a1bc4c2090ad827fa69c587e39e972090778
Gwendal <gwendou95@hotmail.fr>
2022-06-12 17:25:17 +0200

    style(products): clean up code

--- src/components/Recipes/UnPublishRecipe.tsx
+++ src/components/Recipes/UnPublishRecipe.tsx
@@ -59,7 +59,6 @@ const UnPublishRecipe: React.FC<UnPublishRecipeProps> = ({
               {t('recipes.unpublished.title')}
             </h3>
             <div className="mt-2">
-              {/*<p className="text-sm text-gray-500">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>*/}
               <p className="text-sm text-gray-500">
                 {t('recipes.unpublished.message')}
               </p>
