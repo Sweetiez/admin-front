5bc4edd860f0df8b8c57f0261928ddc24533e9ae
LAymeric <aymeric.larvet@gmail.com>
2022-06-23 10:51:54 +0200

    refactor(admin-front): fix prettier

--- src/hooks/ingredients/ingredientsHooks.ts
+++ src/hooks/ingredients/ingredientsHooks.ts
@@ -1,22 +1,22 @@
-import CreateIngredientRequest from "../ingredients/requests/CreateIngredientRequest";
-import {authenticatedRequest} from "../common/request";
-import {useQuery} from "react-query";
-import Ingredient from "../../components/Products/models/Ingredient";
+import CreateIngredientRequest from '../ingredients/requests/CreateIngredientRequest';
+import { authenticatedRequest } from '../common/request';
+import { useQuery } from 'react-query';
+import Ingredient from '../../components/Products/models/Ingredient';
 
 export function useIngredients() {
-    return useQuery<Ingredient[], Error>(`all-ingredients`, async () => {
-        const { data } = await authenticatedRequest({
-            url: `admin/ingredients`,
-        });
-        return data;
+  return useQuery<Ingredient[], Error>(`all-ingredients`, async () => {
+    const { data } = await authenticatedRequest({
+      url: `admin/ingredients`,
     });
+    return data;
+  });
 }
 
 export async function createIngredient(request: CreateIngredientRequest) {
-    const { data } = await authenticatedRequest({
-        url: `/admin/ingredients`,
-        method: 'POST',
-        data: request,
-    });
-    return data;
+  const { data } = await authenticatedRequest({
+    url: `/admin/ingredients`,
+    method: 'POST',
+    data: request,
+  });
+  return data;
 }
