1ffc57ea6c68b6e07aa1903d9d0b4c1b1d324b72
Gwendal <gwendou95@hotmail.fr>
2022-07-03 13:31:52 +0200

    fix(event): date format

--- src/hooks/utils/strings.ts
+++ src/hooks/utils/strings.ts
@@ -1,3 +1,7 @@
 export function capitalizeFirstLetter(str: string): string {
   return str.charAt(0).toUpperCase() + str.substring(1);
 }
+
+export function formatDate(date: string): string {
+  return date.replace('T', ' ').replace('.000Z', '');
+}
