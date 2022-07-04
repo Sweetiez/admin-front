107665e5946dc85ba42acac85a9149a5abfa12d8
Gwendal <gwendou95@hotmail.fr>
2022-07-04 15:38:19 +0200

    style: prettier format

--- src/hooks/users/request/UpdateRoleRequest.ts
+++ src/hooks/users/request/UpdateRoleRequest.ts
@@ -1,10 +1,9 @@
-import RoleModel from "../../../components/Users/models/RoleModel";
+import RoleModel from '../../../components/Users/models/RoleModel';
 
 export default class UpdateRoleRequest {
-    roles: RoleModel[];
+  roles: RoleModel[];
 
-
-    constructor(roles: RoleModel[]) {
-        this.roles = roles;
-    }
+  constructor(roles: RoleModel[]) {
+    this.roles = roles;
+  }
 }
